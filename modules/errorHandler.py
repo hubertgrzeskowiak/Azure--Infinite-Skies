abe595fa8c4dfa762ccc02159fb2e0cf28e4b325
nemesis13 <nemesis13@eb93bf90-6ec9-4721-9d80-a43b5d1d3682>
2009-02-21 23:31:54 +0000

    new coding style(see forums): errorHandler.py (2nd try)
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/azure/azure@42 eb93bf90-6ec9-4721-9d80-a43b5d1d3682

--- modules/errorHandler.py
+++ modules/errorHandler.py
@@ -13,57 +13,57 @@ err_action = RAISE
 
 def setErrAction(actionNum):
     """Sets error handling option"""
-	global err_action
-	err_action = actionNum
+    global err_action
+    err_action = actionNum
 
 def handleError(error):
-	"""Handles an AzureError exception based on some configuration settings"""
-	# NOTE: This subroutine should only be used for handling non-critical errors
-	# (i.e. when the game can still more or less function).  Other, more serious
-	# errors, should be handled manually
-	
-	if err_action == IGNORE_ALL:
-		pass
-	elif err_action == IGNORE:
-		sys.stderr.write(error.message)
-	elif err_action == RAISE:
-		raise error
-	elif err_action == DIE:
-		sys.stderr.write(error.message)
-		sys.exit(1)
-	else:
-		raise ParamError("Error when raising error! Invalid value for handleError(): %d" % err_action)
+    """Handles an AzureError exception based on some configuration settings"""
+    # NOTE: This subroutine should only be used for handling non-critical errors
+    # (i.e. when the game can still more or less function).  Other, more serious
+    # errors, should be handled manually
+    
+    if err_action == IGNORE_ALL:
+        pass
+    elif err_action == IGNORE:
+        sys.stderr.write(error.message)
+    elif err_action == RAISE:
+        raise error
+    elif err_action == DIE:
+        sys.stderr.write(error.message)
+        sys.exit(1)
+    else:
+        raise ParamError("Error when raising error! Invalid value for handleError(): %d" % err_action)
 
 class AzureError(Exception):
-	pass
+    pass
 
 class ResourceLoadError(AzureError):
-	"""Should be thrown when a resource is improperly loaded or fails to load
-	at all"""
-	
-	def __init__(self, resource="None", details="None"):
-		self.message = "Failed to load resource: %s\n\tDetails: %s" % (resource, details)
-		self.resource = resource
-		
-	def __str__(self):
-		return self.message
-		
+    """Should be thrown when a resource is improperly loaded or fails to load
+    at all"""
+    
+    def __init__(self, resource="None", details="None"):
+        self.message = "Failed to load resource: %s\n\tDetails: %s" % (resource, details)
+        self.resource = resource
+        
+    def __str__(self):
+        return self.message
+        
 class ResourceHandleError(AzureError):
-	"""Should be thrown when an attempt is made to use a resource in a way
-	that was not intended"""
-	
-	def __init__(self, resource="None", details="None"):
-		self.message = "Error handling resource: %s\n\tDetails: %s" % (resource, details)
-		self.resource = resource
-		
-	def __str__(self):
-		return self.message
-		
+    """Should be thrown when an attempt is made to use a resource in a way
+    that was not intended"""
+    
+    def __init__(self, resource="None", details="None"):
+        self.message = "Error handling resource: %s\n\tDetails: %s" % (resource, details)
+        self.resource = resource
+        
+    def __str__(self):
+        return self.message
+        
 class ParamError(AzureError):
-	"""Should be thrown when an invalid value is given to a function"""
-	
-	def __init__(self, message=None):
-		self.message = message
-		
-	def __str__(self):
-		return self.message
+    """Should be thrown when an invalid value is given to a function"""
+    
+    def __init__(self, message=None):
+        self.message = message
+        
+    def __str__(self):
+        return self.message
