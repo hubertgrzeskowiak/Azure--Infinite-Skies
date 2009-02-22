cb550fa77a793df127e997d530e7e4c1ca586681
nemesis13 <nemesis13@eb93bf90-6ec9-4721-9d80-a43b5d1d3682>
2009-02-22 00:01:17 +0000

    new coding style(see forums): interface.py (2nd try)
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/azure/azure@44 eb93bf90-6ec9-4721-9d80-a43b5d1d3682

--- modules/interface.py
+++ modules/interface.py
@@ -1,14 +1,14 @@
 """Module containing graphical user interface objects"""
 
 def printInstructions(instructions = ""):
-	"""Give me some text and i'll print it at the top left corner"""
+    """Give me some text and i'll print it at the top left corner"""
     # temporary function. used until we have some real interface
-	from direct.gui.DirectGui import OnscreenText
-	from pandac.PandaModules import TextNode
-	OnscreenText(
-		text = instructions,
-		style = 1,
-		fg = (1,1,1,1),
-		pos = (-1.3, 0.95),
-		align = TextNode.ALeft,
-		scale = .05)
+    from direct.gui.DirectGui import OnscreenText
+    from pandac.PandaModules import TextNode
+    OnscreenText(
+        text = instructions,
+        style = 1,
+        fg = (1,1,1,1),
+        pos = (-1.3, 0.95),
+        align = TextNode.ALeft,
+        scale = .05)
