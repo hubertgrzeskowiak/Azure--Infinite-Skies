92957c6f29b45dda8e2901a8557fd36680199764
gjmm <gjmm@eb93bf90-6ec9-4721-9d80-a43b5d1d3682>
2009-03-15 10:37:53 +0000

    Options parsing separated into new module
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/azure/azure@63 eb93bf90-6ec9-4721-9d80-a43b5d1d3682

--- main.py
+++ main.py
@@ -45,56 +45,18 @@ ESC - quit
 
 import sys
 import os
-import optparse
 
-#------------------------------------------------------------------------------
-# Parser Function
-#------------------------------------------------------------------------------
-
-# Unfortunately, all this has to be defined before we load the Panda3D 
-#       libraries or we get the unfortunate side-effect of the screen
-#       window popping up before options are parsed.
-
-# constants for -v,-d and -q options are found in errors module
-from modules.errors import *
-
-def createOptionParser():
-    "Creates a the parser object an return it"
-    
-    # Create a new parser
-    parser = optparse.OptionParser()
-
-    # Add all options
-    parser.add_option("-v", "--verbose", action = "store_const", const = RAISE,             
-        dest = "verbose", help = "print extra information")
-    parser.set_defaults(verbose = RAISE)
-    
-    parser.add_option("-d","--debug", action = "store_const", const = DIE,
-        dest = "verbose", help = "print extra debugging information")
-        
-    parser.add_option("-q","--quiet", action = "store_const", 
-        const = IGNORE_ALL, dest = "verbose", 
-        help = "do not print information")
-
-    # temporary flags to test camera modes and physics
-    parser.add_option("-g","--ghost", 
-        action="store_true", dest="ghost", default=False,
-        help="use ghost flying mode")
-    parser.add_option("-o","--oldphysics", 
-        action="store_true", dest="oldphysics", default=False,
-        help="use old physics")
-
-    
-    return parser
-
-# Parse options
-parser = createOptionParser()
-options, args = parser.parse_args()
-setErrAction(options.verbose)
 
 #------------------------------------------------------------------------------
 # panda3d modules
 #------------------------------------------------------------------------------
+
+# quick diversion to process the command line options.
+# options are processed on loading the module
+# must be loaded before direct.directbase.DirectStart
+import modules.options as opts
+# options will be stored in opts.options
+
 
 # some panda configuration settings
 from pandac.PandaModules import loadPrcFileData
@@ -289,7 +251,7 @@ class Azure(object):
 def main():
     
     # Create new azure object
-    azure = Azure(options)
+    azure = Azure(opts.options)
     azure.startGame()
 
 #------------------------------------------------------------------------------
