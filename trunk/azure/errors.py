686fd3de41e6c51a4c3300bfefdab0685b882911
nemesis13 <nemesis13@eb93bf90-6ec9-4721-9d80-a43b5d1d3682>
2009-07-22 15:08:53 +0000

    simple and dirty way for merging
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/azure/azure@86 eb93bf90-6ec9-4721-9d80-a43b5d1d3682

---
+++ trunk/azure/errors.py
@@ -0,0 +1,69 @@
+"""Error handling stuff"""
+
+import sys
+
+# Constants used for the err_action variable
+IGNORE_ALL, IGNORE, RAISE, DIE = range(4)
+
+# IGNORE_ALL - Ignore an AzureError, and don't print AzureError messages
+# IGNORE - Ignore an AzureError, but print AzureError messages
+# RAISE - Keep the thrown AzureError raised for some other code to handle
+# DIE - Die on a thrown AzureError; 
+err_action = RAISE
+
+def setErrAction(actionNum):
+    """Sets error handling option"""
+    global err_action
+    err_action = actionNum
+
+def handleError(error):
+    """Handles an AzureError exception based on some configuration settings"""
+    # NOTE: This subroutine should only be used for handling non-critical errors
+    # (i.e. when the game can still more or less function).  Other, more serious
+    # errors, should be handled manually
+    
+    if err_action == IGNORE_ALL:
+        pass
+    elif err_action == IGNORE:
+        sys.stderr.write(error.message)
+    elif err_action == RAISE:
+        raise error
+    elif err_action == DIE:
+        sys.stderr.write(error.message)
+        sys.exit(1)
+    else:
+        raise ParamError("Error when raising error! Invalid value for handleError(): %d" % err_action)
+
+class AzureError(Exception):
+    pass
+
+class ResourceLoadError(AzureError):
+    """Should be thrown when a resource is improperly loaded or fails to load
+    at all"""
+    
+    def __init__(self, resource="None", details="None"):
+        self.message = "Failed to load resource: %s\n\tDetails: %s" % (resource, details)
+        self.resource = resource
+        
+    def __str__(self):
+        return self.message
+        
+class ResourceHandleError(AzureError):
+    """Should be thrown when an attempt is made to use a resource in a way
+    that was not intended"""
+    
+    def __init__(self, resource="None", details="None"):
+        self.message = "Error handling resource: %s\n\tDetails: %s" % (resource, details)
+        self.resource = resource
+        
+    def __str__(self):
+        return self.message
+        
+class ParamError(AzureError):
+    """Should be thrown when an invalid value is given to a function"""
+    
+    def __init__(self, message=None):
+        self.message = message
+        
+    def __str__(self):
+        return self.message
---
+++ trunk/azure/options.py
@@ -0,0 +1,43 @@
+"""Command line arguments handling."""
+
+import optparse
+
+# constants for -v,-d and -q options are found in errors module
+from errors import *
+
+def createOptionParser():
+    """Creates a parser object and returns it. To call any options' values look
+    for this object's attributes."""
+    
+    # Create a new parser
+    parser = optparse.OptionParser()
+
+    # Add all options
+    parser.add_option("-v", "--verbose", action="store_const", const=RAISE,
+        dest = "verbose", help="print extra information")
+    parser.set_defaults(verbose=RAISE)
+    
+    parser.add_option("-d","--debug", action="store_const", const=DIE,
+        dest="verbose", help="print extra debugging information")
+        
+    parser.add_option("-q","--quiet", action="store_const",
+        const=IGNORE_ALL, dest="verbose",
+        help="do not print information")
+
+    # temporary flags to test camera modes and physics
+    parser.add_option("-g","--ghost", 
+        action="store_true", dest="ghost", default=False,
+        help="use ghost flying mode")
+    parser.add_option("-o","--oldphysics", 
+        action="store_true", dest="oldphysics", default=False,
+        help="use old physics")
+    parser.add_option("-a","--autolevel", 
+        action="store_true", dest="autolevel", default=False,
+        help="allow autolevel code")
+
+    return parser
+
+# Parse options
+parser = createOptionParser()
+options, args = parser.parse_args()
+setErrAction(options.verbose)
