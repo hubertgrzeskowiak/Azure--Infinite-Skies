2dae9e8cbbb34aca509a483215db67d74ae7ff7f
nemesis13 <nemesis13@eb93bf90-6ec9-4721-9d80-a43b5d1d3682>
2009-11-02 20:41:52 +0000

    new big views module. featuring automatic camera detection from egg files and toggable camera mode :)
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/azure/azure@112 eb93bf90-6ec9-4721-9d80-a43b5d1d3682

--- trunk/azure/controls.py
+++ trunk/azure/controls.py
@@ -1,16 +1,21 @@
 """Module for everything about controls."""
 
 import sys
+
 from direct.showbase.DirectObject import DirectObject
 from direct.fsm.FSM import FSM
 from direct.task import Task
-import views
+from direct.directnotify.DirectNotify import DirectNotify
+
+from views import PlaneCamera
+
 
 class KeyHandler(DirectObject):
     """Gets key events and does stuff."""
 
     def __init__(self):
         self.key_states = {}
+        self.notify = DirectNotify().newCategory("azure-control")
 
     def chKeyState(self, key, value):
         #print("key %s changed to %d" % (key, value))
@@ -20,9 +25,13 @@ class KeyHandler(DirectObject):
         """Mount a controlmap dict."""
         self.controlmap = controlmap
 
+        self.plane_camera = PlaneCamera(base.player)
+        #self.plane_camera.request("ThirdPerson")
         # non-continous keys (direct action, no state saving)
         self.accept("escape", sys.exit)
-        
+        self.accept("c", self.plane_camera.requestNext)
+        self.accept("shift-c", self.plane_camera.requestPrev)
+
         # continuous keys
         for key in self.controlmap:
             # at start no key is active
@@ -31,7 +40,7 @@ class KeyHandler(DirectObject):
             self.accept(key, self.chKeyState, [key, 1])
             self.accept(key+"-up", self.chKeyState, [key, 0])
 
-        
+
 class Controlmaps(object):
     """Some control maps."""
 
@@ -44,10 +53,11 @@ class Controlmaps(object):
         "e": ("move", "heap-left"),
         "page_up": ("thrust", "add"),
         "page_down": ("thrust", "subtract"),
-        "p": ("cam-view", views.FIRST_PERSON),
-        "o": ("cam-view", views.COCKPIT),
-        "i": ("cam-view", views.THIRD_PERSON),
-        "u": ("cam-view", views.DETACHED)
+        #"c" : ("camera", "next")
+        #"p": ("cam-view", views.FIRST_PERSON),
+        #"o": ("cam-view", views.COCKPIT),
+        #"i": ("cam-view", views.THIRD_PERSON),
+        #"u": ("cam-view", views.DETACHED)
         }
 
 
@@ -55,18 +65,19 @@ class ControlManager(FSM):
     """A finite state machine which determines current controls and player
     status."""
     def __init__(self):
-        FSM.__init__(self, "playerFSM")
+        FSM.__init__(self, "Control Manager for player")
         self.k = KeyHandler()
 
     def enterFly(self):
         self.k.mountControlmap(Controlmaps.flight)
-        self.fly_task = taskMgr.add(self.flyTask, "flyTask")
+        self.fly_task = taskMgr.add(self.__flyTask, "flyTask")
+
     def exitFly(self):
         pass
         #TODO: implement this..
 
 
-    def flyTask(self, task):
+    def __flyTask(self, task):
         """Move the plane acording to pressed keys."""
         for key, state in self.k.key_states.items():
             if state == 1:
@@ -76,6 +87,7 @@ class ControlManager(FSM):
                     base.player.move(key_info[1])
                 elif key_info[0] == "thrust":
                     base.player.chThrust(key_info[1])
+
         #base.player.reverseRoll()
         #base.player.reversePitch()
         base.player.velocityForces()
--- trunk/azure/views.py
+++ trunk/azure/views.py
@@ -1,75 +1,234 @@
 """Camera(s) handling module"""
 
-from pandac.PandaModules import ClockObject
+import string
+
+from direct.fsm.FSM import FSM
+#from pandac.PandaModules import ClockObject
+from pandac.PandaModules import NodePathCollection, NodePath
 from direct.directnotify.DirectNotify import DirectNotify
 from direct.task import Task
-from errors import *
 
-c = ClockObject.getGlobalClock()
+from errors import *
+from aircrafts import Aeroplane
+
 
 # View mode constants
 FIRST_PERSON, COCKPIT, THIRD_PERSON, DETACHED = range(4)
 
-class PlaneCamera(object):
-    """Player's plane camera management"""
+class PlaneCamera(FSM):
+    """Give this class a plane as argument and it will create
+    some nodes around it which you can parent the camera to (if there are no
+    such nodes yet). Keep in mind, that it only uses base.camera the whole
+    time - no other cams are involved.
 
-    def __init__(self, parent, view_mode=THIRD_PERSON):
+    Usage:
+    plane_camera = PlaneCamera(aeroplane)
+    plane_camera.setCameraMode("ThirdPerson")
+    plane_camera.setCameraMode("Next")
+    """
+    def __init__(self, parent):
         """Arguments:
-        parent -- object which the camera should follow
-        view_mode -- available modes are FIRST_PERSON, COCKPIT, THIRD_PERSON
-                     and DETACHED. THIRD_PERSON is default. modes are
-                     importable from this module"""
-        
-        self.notify = DirectNotify().newCategory("azure-camera")
-        assert base
-        self.camera = base.camera
-        self.parent = parent        
-        self.setViewMode(view_mode)
-        
-    def getViewMode(self):
-        """Returns the current view mode"""
+        parent -- Aeroplane which the camera should follow
+        """
+
+        # Used for debugging. Verbosity is set in config file.
+        # Usually this is called self.notify, but in this case it would
+        # override FSM's own.
+        self.notifier = DirectNotify().newCategory("azure-camera")
+        self.parent = parent
+        # Replaced by a NodePath with all available cameras as children and
+        # plane node as parent.
+        self.__cameras = None
+
+        #if parent.__class__.__name__ is not "Aeroplane":
+        if not isinstance(self.parent, Aeroplane):
+            raise ParamError, "Parent must be an Aeroplane instance, but is %s" % type(self.parent)
+
+        FSM.__init__(self, "PlaneCamera: %s" % self.parent.name)
+
         try:
-            return self.__view_mode
+            self.camera = base.camera
         except:
-            return None
-        
-    def setViewMode(self, view_mode):
-        """Sets camera view mode. Takes a view_mode constant as argument."""
+            raise BaseMissing
 
-        if view_mode != self.getViewMode():
-            self.notify.info("setting view to %s" % view_mode)
+        self.__cameras = self.parent.node().find("cameras")
+        if not self.__cameras:
+            self.createCamNodes()
+        self.updateCamArray()
+
+
+        # Set up the default camera
+        #self.setCameraMode("ThirdPerson")
+        #self.request("ThirdPerson")
+        base.accept("1", self.request, ["ThirdPerson"])
+        base.accept("2", self.request, ["FirstPerson"])
+        base.accept("3", self.request, ["Cockpit"])
+
+    def createCamNodes(self):
+        """Creates a few empty nodes around a plane which the camera might be
+        parented to. It looks if there are cameras inside the model file and
+        uses those if possible. Where everything named "camera CamType" is
+        considered a camera. At least ThirdPerson, FirstPerson and Cockpit
+        should be defined inside the egg file, otherwise some guessed defaults
+        are taken.
+        """
+
+        # Look for cameras inside the model (loaded egg file)
+        cameras = NodePath("cameras")
+        _c = self.parent.node().findAllMatches("**/camera ?*")
+        _c.removeDuplicatePaths()
+        _c.reparentTo(cameras)
+
+        if not cameras.isEmpty():
+            self.notifier.info("Cameras found under model:\n%s"
+                               % _c)
         else:
-            self.notify.info("view already is %s. doing nothing" % view_mode)
-            return
+            self.notifier.info("No cameras found under model.")
 
-        if view_mode == FIRST_PERSON:
-            # plane specific - later on managable with emptys or config-vars.
-            self.camera.reparentTo(self.parent)
-            self.camera.setPosHpr(-1.6, 3.3, 1.2, 0, -7, 0)
-            
-        elif view_mode == COCKPIT:
-            # plane specific - later on managable with emptys or config-vars.
-            # buggy because of solid, one-sided textures.
-            self.camera.reparentTo(self.parent)
-            self.camera.setPosHpr(0, -1.5, 1.75, 0, 0, 0)
+        # FirstPerson camera is a must-have. Set up a guessed one if none
+        # defined yet.
+        if not cameras.find("camera FirstPerson"):
+            assert self.notifier.debug("No first person camera found in %s. "
+                                  "Guessing best position." % self.parent.name)
+            first_person = NodePath("camera FirstPerson")
+            # TODO: Guess best position based on bounding box.
+            first_person.setY(5)
+            first_person.reparentTo(cameras)
 
-        elif view_mode == THIRD_PERSON:
-            # should make use of aircraft bounds (see aircrafts.py)
-            self.camera.reparentTo(self.parent)
-            self.camera.setPosHpr(0, -25, 8, 0, -7, 0)
-            
-        elif view_mode == DETACHED:
-            self.camera.reparentTo(render)
-            self.camera.setPos(0, 0, 20)
-            # rotation set by lookAt()
-            
+        # ThirdPerson camera is a must-have. Set up a guessed one if none
+        # defined yet.
+        if not cameras.find("camera ThirdPerson"):
+            assert self.notifier.debug("No third person camera found in %s. "
+                                  "Guessing best position." % self.parent.name)
+            third_person = NodePath("camera ThirdPerson")
+            # TODO: Guess best position based on bounding box.
+            third_person.setPos(0, -20, 3)
+            third_person.setP(-80)
+            third_person.reparentTo(cameras)
+
+        # Cockpit needs to be accurate. Don't try to guess it.
+        if not cameras.find("camera Cockpit"):
+            assert self.notifier.debug("No cockpit camera found in "
+                                       "%s. Cockpit camera disabled."
+                                       % self.parent.name)
+
+        self.__cameras = cameras
+        # Store the cams at parent node..
+        # You can edit the camera nodes from outside as well.
+        # If you attach new camera nodes, though, you'll have to call this
+        # function again.
+        cameras.reparentTo(self.parent.node())
+
+    def updateCamArray(self, cameramodes=None):
+        """Set the cameras which next and previous will switch to. Expects a
+        list or tuple. Defaults to all found cameras."""
+        a = []
+        if not cameramodes:
+            for c in self.__cameras.getChildren():
+                if c.getName().startswith("camera "):
+                    a.append(c.getName().strip("camera "))
+            self.setStateArray(a)
         else:
-            self.notify.warning("%s view mode unknown" % view_mode)
-            raise ParamError("Unknown Option: %s" % view_mode)
-            
-        self.__view_mode = view_mode
-        
-    def _update_cam(self, task):
-        """updates camera position and rotation"""
-        if self.__view_mode == DETACHED:
-            self.camera.lookAt(self.parent)
+            self.setStateArray(cameramodes)
+
+
+    def getCameraMode(self):
+        """Returns the current view mode."""
+        return self.getCurrentOrNextState()
+
+    def setCameraMode(self, mode, *args):
+        """Convenience function."""
+        return self.request(mode, args)
+
+    def defaultEnter(self, *args):
+        """Executed by the FSM every time an undefined state is entered.
+        Note: this function is called AFTER the responsible filter."""
+
+        assert self.notifier.debug("Changing state from %s to %s with args: %s."
+                                   % (self.oldState, self.newState, args))
+        request = self.newState
+
+        target_cam = self.__cameras.find("camera " + request)
+        if target_cam:
+            try:
+                self.camera.reparentTo(target_cam)
+            except:
+                self.notifier.warning(
+                        "Ok, now this really shouldn't happen! Filter said the"
+                        "camera is there and enter can't find it...")
+
+
+    def defaultFilter(self, request, args):
+        """Executed by the FSM every time an undefined state is requested."""
+        assert self.notifier.debug("Requested %s with args: %s"
+                                   % (request, args))
+        if request == "Next":
+            return self.requestNext(args)
+        if request == "Prev":
+            return self.requestPrev(args)
+        if request == "Detached":
+            return (request,) + args
+        if self.__cameras.find("camera " + request):
+            # TODO(Nemesis13, 26.10.09): add some nice camera transition
+            return (request,) + args
+        assert self.notifier.info("Sorry, no %s camera found." % request)
+        return None
+
+    def filterOff(self, request, args):
+        """Override the default filter which allows all states from Off."""
+        return self.defaultFilter(request, args)
+
+    def requestNext(self, *args):
+        """Request the 'next' state in the predefined state array."""
+        self.fsmLock.acquire()
+        try:
+            if self.stateArray is not []:
+                if not self.state in self.stateArray:
+                    self.request(self.stateArray[0])
+                else:
+                    cur_index = self.stateArray.index(self.state)
+                    new_index = (cur_index + 1) % len(self.stateArray)
+                    self.request(self.stateArray[new_index], args)
+            else:
+                assert self.notifier.debug(
+                                    "stateArray empty. Can't switch to next.")
+
+        finally:
+            self.fsmLock.release()
+
+    def requestPrev(self, *args):
+        """Request the 'previous' state in the predefined state array."""
+        self.fsmLock.acquire()
+        try:
+            if self.stateArray is not []:
+                if not self.state in self.stateArray:
+                    self.request(self.stateArray[0])
+                else:
+                    cur_index = self.stateArray.index(self.state)
+                    new_index = (cur_index - 1) % len(self.stateArray)
+                    self.request(self.stateArray[new_index], args)
+            else:
+                assert self.notifier.debug(
+                                    "stateArray empty. Can't switch to next.")
+        finally:
+            self.fsmLock.release()
+
+    # Extra States:
+
+
+    def enterDetached(self):
+        """Lets the camera view the plane from far away."""
+        self.camera.wrtReparentTo(render)
+        taskMgr.add(__detachedCam, "detached camera transforms")
+
+    def exitDetached(self):
+        taskMgr.remove(__detachedCam)
+
+    def __detachedCam(self, task):
+        """Updates camera position and rotation."""
+        try:
+            self.camera.lookAt(self.parent.node())
+        except:
+            assert self.notifier.warning("Error on detached cam task. Exit.")
+            return Task.done
+        return Task.cont
