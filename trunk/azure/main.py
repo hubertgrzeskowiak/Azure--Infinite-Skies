23ccf93c8a2fa487789e6ca7557d62df1ba5b35e
nemesis13 <nemesis13@eb93bf90-6ec9-4721-9d80-a43b5d1d3682>
2009-11-02 13:06:02 +0000

    cleanup
    git-svn-id: svn+ssh://svn.tuxfamily.org/svnroot/azure/azure@111 eb93bf90-6ec9-4721-9d80-a43b5d1d3682

--- trunk/azure/main.py
+++ trunk/azure/main.py
@@ -18,51 +18,37 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, see <http://www.gnu.org/licenses/>.
 """
 
-INSTRUCTIONS = """\
-Azure testing ground
-a/d - roll
-w/s - pitch
-q/e - yaw
-z/c - move cam
-x - reset cam
-
-space - move forward
-PageUp - increase thrust
-PageDown - decrease thrust
-
-VIEWS
-p - first person
-o - cockpit
-i - third person
-u - detached
-
-ESC - quit
-"""
-
 import sys
 import os
 # Here panda will look for files with the ending .prc and load them as config.
 # The config file will be loaded as soon as you touch a panda module.
 #os.putenv("PANDA_PRC_PATH", "./etc/:../etc/:/etc/azure/:~/.azure/")
+# Ffor now, load only our development config. First 2 lines are old-style,
+# latter 2 for panda 1.7 and above.
+os.environ["PANDA_PRC_DIR"] = os.getcwd()
+os.environ["PANDA_PRC_PATH"] = os.getcwd()
+os.environ["PRC_DIR"] = os.getcwd()
+os.environ["PRC_PATH"] = os.getcwd()
 
-# NOT WORKING! don't ask me why..
-os.environ["PRC_PATH"] = 'etc/:../etc/:/etc/azure/'
-os.environ["PANDA_PRC_PATH"] = 'etc/:../etc/:/etc/azure/'
-# TEMPORARY WORKAROUND:
 try:
     from pandac.PandaModules import loadPrcFile
 except ImportError:
     print "It seems you haven't got Panda3D installed properly."
     sys.exit(1)
+# Just for the case setting the environment variable setting failed, load the
+# config file now. It can'T overwrite everything from the beginning anymore,
+# but should work well as a fallback.
 loadPrcFile("etc/azure.prc")
+
 
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
+from pandac.PandaModules import *
 
 
 class Azure(object):
     """Main class called by the top level main function (see below)."""
-    
+
     def __init__(self):
         """Only argument is the options object from the same named module."""
 
@@ -70,18 +56,12 @@ class Azure(object):
         ShowBase()
         base.disableMouse()
 
-
-        print 20*"#"
-        print "config path(s):"
-        print cpMgr.getSearchPath()
-        print 20*"#"
-        print "importing TestEnvironment"
         from scenarios import TestEnvironment
-        print "initialising TestEnvironment"
         TestEnvironment()
-        
+
         run()
 
+# Related to relative paths.
 if __name__ == "__main__":
     print "Don't run this module directly! Use the run script instead!"
     sys.exit(2)
