7fc18236deb1073155e014e5c0aa3d9a0a3a17c6
nemesis13 <dev at nemesis13.de>
2010-06-11 20:07:32 +0200

    water surface. crappy, but the best you can get without shaders. Closes #14
    Signed-off-by: nemesis13 <dev at nemesis13.de>

--- azure/scenery.py
+++ azure/scenery.py
@@ -1,6 +1,7 @@
 """This module manages everthing about non-interactive environment."""
 
 from pandac.PandaModules import *
+from direct.showbase.DirectObject import DirectObject
 
 from errors import *
 
@@ -73,6 +74,17 @@ class Scenery(object):
         return self._dummy_node
 
 
+#class Sky2(NodePath):
+#    def __init__(self, *args):
+#        self = loader.loadModel("textured_sky")
+#        self.setScale(10000)
+#        self.setBin('background', 0)
+#        self.setDepthTest(False)
+#        self.setDepthWrite(False)
+#        self.setLightOff()
+#        self.setCompass()
+#        self.reparentTo(camera)
+
 class Sky(NodePath):
     """Skies are implemented in form of a cube with 6 square textures. See
     assets/skyboxes/README.txt for more details."""
@@ -95,7 +107,7 @@ class Sky(NodePath):
         self = loader.loadModel("misc/invcube")
         self.clearTexture()
         self.clearMaterial()
-        self.setScale(100)
+        self.setScale(10000)
         self.setTwoSided(True)
         self.setBin('background', 0)
         self.setDepthTest(False)
@@ -104,5 +116,108 @@ class Sky(NodePath):
         self.setTexGen(TextureStage.getDefault(), TexGenAttrib.MWorldPosition)
         self.setTexProjector(TextureStage.getDefault(), render, self);
         self.setTexture(tex, 1)
-        self.setCompass()
-        self.reparentTo(camera)
+        #self.setCompass()  # not needed with world-space-UVs
+        self.reparentTo(render)
+
+class Water(NodePath, DirectObject):
+    def __init__(self, size=10000, resoltion=1024):
+        """Arguments:
+        size -- Edge length of the water square.
+        resolution -- Texture size of the rendered reflection buffer.
+        """
+        NodePath.__init__(self, "water")
+        DirectObject.__init__(self)
+        self.cm = CardMaker("water surface")
+        self.cm.setFrame(-0.5, 0.5, -0.5, 0.5)
+        self.cm.setHasUvs(True)
+        self.attachNewNode(self.cm.generate())
+        self.reparentTo(render)
+        self.setScale(size)
+        self.setP(self, -90)
+        self.flattenLight()
+        self.hide(BitMask32.bit(1))
+        #self.setTwoSided(True)
+
+        diffuse = loader.loadTexture("textures/water.diffuse.png")
+        diffuse.setWrapU(Texture.WMRepeat)
+        diffuse.setWrapV(Texture.WMRepeat)
+        diffuse.setMinfilter(Texture.FTLinearMipmapLinear)
+        diffuse.setMagfilter(Texture.FTLinearMipmapLinear)
+        diffuse_stage = TextureStage("diffuse")
+        diffuse_stage.setSort(2)
+        self.setTexture(diffuse_stage, diffuse)
+        self.setTexScale(diffuse_stage, size/100, size/100)
+        self.diffuse_stage = diffuse_stage
+
+        #normal = loader.loadTexture("textures/water.normal.png")
+        #normal.setWrapU(Texture.WMRepeat)
+        #normal.setWrapV(Texture.WMRepeat)
+        #normal.setMinfilter(Texture.FTLinear)
+        #normal.setMagfilter(Texture.FTLinear)
+        #normal_stage = TextureStage("normal")
+        #normal_stage.setMode(TextureStage.MNormal)
+        #normal_stage.setSort(3)
+        #self.setTexture(normal_stage, normal)
+        #self.setTexScale(normal_stage, 100, 100)
+        #self.normal_stage = normal_stage
+
+        # Required for normal maps.
+        #self.setShaderAuto()
+
+        buffer = base.win.makeTextureBuffer("water reflection", 1024, 1024)
+        buffer.setClearColor(Vec4(0, 0, 0, 1))
+        
+        # Reflection camera renders to 'buffer' which is projected onto the
+        # water surface.
+        self.refl_cam = base.makeCamera(buffer)
+        self.refl_cam.reparentTo(render)
+        self.refl_cam.node().setCameraMask(BitMask32.bit(1))
+        self.refl_cam.node().getLens().setFov(base.camLens.getFov())
+        self.refl_cam.node().getLens().setNearFar(1,100000)
+        
+        plane = PlaneNode("water culling plane",
+                          Plane(Vec3(0, 0, 1), Point3(0, 0, 0)))
+        cfa = CullFaceAttrib.makeReverse()
+        cpa = ClipPlaneAttrib.make(PlaneNode.CEVisible, plane)
+        rs = RenderState.make(cfa, cpa)
+        self.refl_cam.node().setInitialState(rs)
+        
+        reflection = buffer.getTexture()
+        reflection.setMinfilter(Texture.FTLinear)
+        reflection.setMagfilter(Texture.FTLinear)
+        refl_stage = TextureStage("reflection")
+        refl_stage.setSort(1)
+        self.refl_stage = refl_stage
+        self.projectTexture(refl_stage, reflection, base.cam)
+        self.setTexture(refl_stage, reflection)
+
+        # Blend between diffuse and reflection.
+        diffuse_stage.setColor(VBase4(1, 1, 1, 0.2))  # opacity of 20%
+        diffuse_stage.setCombineRgb(TextureStage.CMInterpolate,
+                TextureStage.CSTexture, TextureStage.COSrcColor,
+                TextureStage.CSPrevious, TextureStage.COSrcColor,
+                TextureStage.CSConstant, TextureStage.COSrcAlpha)
+
+
+        self.addTask(self.update, name="water update", sort=1)
+
+    def update(self, task):
+        """Updates position of the reflection camera and the water plane."""
+        mc = base.cam.getMat(render)
+        #mf = Plane(Vec3(0, 0, 1), Point3(0, 0, 0)).getReflectionMat()
+        mf = Mat4(1, 0, 0, 0,
+                  0, 1, 0, 0,
+                  0, 0, -1, 0,
+                  0, 0, 0, 1)
+        self.refl_cam.setMat(mc * mf)
+        
+        self.setX(camera.getX())
+        self.setY(camera.getY())
+        self.setTexOffset(self.diffuse_stage,
+                self.getX()/self.getTexScale(self.diffuse_stage)[0],
+                self.getY()/self.getTexScale(self.diffuse_stage)[1])
+        #self.setTexOffset(self.normal_stage,
+        #        self.getX()/self.getTexScale(self.normal_stage)[0],
+        #        self.getY()/self.getTexScale(self.normal_stage)[1])
+
+        return task.cont
