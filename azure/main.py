f513ed3ea9108dbbaec526352fc5ee7ea7aba539
nemesis13 <dev at nemesis13.de>
2011-03-21 01:21:05 +0100

    better commandline option handling
    Signed-off-by: nemesis13 <dev at nemesis13.de>

--- azure/main.py
+++ azure/main.py
@@ -1,5 +1,20 @@
+"""Azure: Infinite Skies main file. This module handles a few command line
+options and starts the core finite state machine, followed by Panda's task
+manager."""
+
 import sys
 import os
+
+from options import options
+
+# If we only need to print version, do this first and leave everything else
+# untouched.
+if options.print_version:
+    try:
+        print open("VERSION").read()
+    except IOError:
+        print "Version unknown. Can't find the VERSION file."
+    sys.exit()
 
 try:
     from pandac.PandaModules import loadPrcFile
@@ -13,18 +28,11 @@ loadPrcFile(Filename.fromOsSpecific(os.path.abspath(os.path.join(sys.path[0], "e
 from direct.showbase.ShowBase import ShowBase
 
 from core import Core
-import options
 
 
 class Azure(ShowBase):
     """Main class called by the top level main function (see below)."""
     def __init__(self):
-        if options.print_version:
-            try:
-                print open("VERSION").read()
-            except IOError:
-                print "Version unknown. Can't find the VERSION file."
-            sys.exit()
 
         # This basically sets up our rendering node-tree, some builtins and
         # the master loop (which iterates each frame).
@@ -33,10 +41,7 @@ class Azure(ShowBase):
         self.disableMouse()
         self.setBackgroundColor(0,0,0,1)
         # Start our Finite State Machine
-        if options.scenario:
-            self.core = Core(options.scenario)
-        else:
-            self.core = Core()
+        self.core = Core(options.scenario) if options.scenario else Core()
 
         #base.bufferViewer.toggleEnable()
 		# Start the master loop.
