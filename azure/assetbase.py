331ea86743ed17e3f62faed2dfdd8023d3aebf40
nemesis13 <dev at nemesis13.de>
2011-11-21 03:04:12 +0100

    this could be imported by asset classes to make sure they meet the minimum requirements. think of this as an interface in java terms

---
+++ azure/assetbase.py
@@ -0,0 +1,23 @@
+from panda3d.core import NodePath
+
+class AssetBase(object):
+    """Classes for assets are considered proxies - mostly for models in the
+    scene graph .
+    """
+    def __init__(self):
+        """The constructor should only expect ultimately required parameters.
+        """
+        # Path to the root node.
+        self.node = NodePath()
+
+        # Name of the particular asset.
+        self.name = "(No name given)"
+
+        # Path to the actually visible node. Must be either the same as
+        # self.node or a child of it. This is optional.
+        self.model = NodePath() # can be an Actor alternatively
+
+
+    def destroy(self):
+        """Cleans up everyting this asset created and attached to the SG."""
+        raise NotImplementedError()
