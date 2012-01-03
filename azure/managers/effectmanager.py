30211ff8ca4e0dfb88663469b8d6ec76c4711ce5
nemesis13 <dev at nemesis13.de>
2012-01-03 11:36:42 +0100

    new effect manager WIP

---
+++ azure/managers/effectmanager.py
@@ -0,0 +1,6 @@
+"""Manager for common screen effects like bloom, motion blur, hdr etc."""
+
+TODO: write an interface to common filters and add a few new effects
+
+class EffectManager(object):
+    pass
