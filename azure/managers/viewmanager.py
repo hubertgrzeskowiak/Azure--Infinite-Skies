6217fcd78408ba6e1a9456d6bec8a945f375b719
nemesis13 <dev at nemesis13.de>
2012-01-03 23:32:12 +0100

    renamed cameramanager to viewmanager and added a few comments. also it's now a FSM

---
+++ azure/managers/viewmanager.py
@@ -0,0 +1,55 @@
+from direct.fsm import FSM
+
+from azure import views
+
+
+class ViewManager(FSM):
+    """A view manager holds instances of view classes that control
+    cameras. There is only one active view at a time.
+
+    Although the attributes are not declared as private, this class is meant to
+    be used only through its functions.
+    """
+    def __init__(self, camera):
+        """Arguments:
+        camera -- NodePath to the camera used
+        """
+        # TODO: Set up a log notifier
+        # TODO: make this a finite state machine
+        FSM.__init__(self, "view manager")
+        self.camera = camera
+        self.view = None
+
+    def setView(self, view, *args, **kwargs):
+        """Set a view from views package.
+        Arguments:
+        view -- name of a view class from the views package
+        All other arguments will be passed to the constructor of the view.
+        """
+        # First, remove the old view
+        if self.view is not None:
+            self.view.destroy()
+        if view in dir(views):
+            V = getattr(views, view)
+            v = V(self.camera, *args, **kwargs)
+        else:
+            raise Exception("Couldn't load view: {}".format(view))
+
+    def getView(self):
+        """Get the currently active view class."""
+        return self.view
+
+    def getViewName(self):
+        """Get the name of the currently active view class."""
+        if hasattr(self.view, "__class__"):
+            return self.view.__class__.__name__
+        else:
+            return None
+
+    def destroy(self):
+        """Destroy the active view and clean up this manager. This only resets
+        the manager to its initial state, so it's safe (but unrecommended) to
+        use it after calling this function.
+        """
+        self.view.destroy()
+        self.view = None
