9deed9eed6f878cc3e66d477bb0a7ca6c9e91e70
nemesis13 <dev at nemesis13.de>
2012-02-19 14:02:04 +0100

    scenarios don't get parameters passed like that

--- azure/core.py
+++ azure/core.py
@@ -24,7 +24,7 @@ class Core(FSM):
         # It might get replaced by an own task manager, by chance.
         base.taskMgr.setupTaskChain("scenario", frameBudget=-1)
 
-    def enterLoading(self, scenario, *args):
+    def enterLoading(self, scenario):
         # TODO: put this into gui package and add a black background
         self.loading = OnscreenText(text="LOADING", pos=(0,0), scale=0.1,
                                align=TextNode.ACenter, fg=(1,1,1,1))
@@ -36,19 +36,20 @@ class Core(FSM):
         # self.demand(scenario), but preloadFast() is executed in one frame, so
         # we can safely demand that from here. Interactive loading screens
         # might require special handling.
-        self.demand("Scenario", scenario, *args)
+        self.demand("Scenario", scenario)
 
     def exitLoading(self):
         self.loading.destroy()
         del self.loading
         del self.preloader
 
-    def enterScenario(self, scenario, *args):
-        self.scenario = ScenarioProxy(scenario, *args)
+    def enterScenario(self, scenario):
+        self.scenario = ScenarioProxy(scenario)
         self.scenario.begin()
 
     def exitScenario(self):
         self.scenario.destroy()
+        del self.scenario
 
     def enterMenu(self, menu, *args):
         #self.menu = MenuProxy(menu, *args)
