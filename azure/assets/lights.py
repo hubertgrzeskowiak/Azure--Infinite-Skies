f2688e711e7cc166e27d5c7c16fb119f25bb8536
nemesis13 <dev at nemesis13.de>
2012-02-19 13:56:28 +0100

    added name as first parameter to lights, because all assets take that as first argument now.

--- azure/assets/lights.py
+++ azure/assets/lights.py
@@ -5,19 +5,19 @@ from panda3d.core import NodePath
 from assetbase import AssetBase
 
 class AmbientLight(AssetBase):
-    def __init__(self, color=Vec4(0.6, 0.6, 0.8, 1)):
+    def __init__(self, name="ambient light", color=Vec4(0.6, 0.6, 0.8, 1)):
         self.light = AL("ambient light")
         self.light.setColor(color)
         self.node = NodePath(self.light)
 
 class DirectionalLight(AssetBase):
-    def __init__(self, color=Vec4(1,1,1,1)):
+    def __init__(self, name="directional light", color=Vec4(1,1,1,1)):
         self.light = DL("directional light")
         self.light.setColor(color)
         self.node = NodePath(self.light)
 
 class DefaultLights(AssetBase):
-    def __init__(self):
+    def __init__(self, name="default light"):
         self.ambient = AmbientLight()
         self.sun = DirectionalLight()
 
