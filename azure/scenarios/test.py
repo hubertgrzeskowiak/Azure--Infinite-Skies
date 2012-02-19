4993506a1a75dc18df2efd0bf94f730082a3a345
nemesis13 <dev at nemesis13.de>
2012-02-19 14:06:34 +0100

    do not use sky or water, since they're not yet there. also use real function names from new modules

--- azure/scenarios/test.py
+++ azure/scenarios/test.py
@@ -1,8 +1,8 @@
 class Developmentenvironment(object):
     def __init__(self):
-        name = "Development Environment"
+        self.name = "Development Environment"
         # having such long paths here is so suboptimal..
-        models = ["planes/griffin/griffin",]
+        self.models = ["planes/griffin/griffin",]
 
     def prepare(*args, **kwargs):
         pass
@@ -10,33 +10,19 @@ class Developmentenvironment(object):
     def begin(self, managers):
         m = managers
 
-        m.assets.add("AmbientLight", color=(1.6, 1.6, 1.8, 1))
-        m.assets.add("DirectionalLight", color=(2.0, 1.9, 1.6, 1))
-        m.assets.add("Sky", "bluesky")
-        m.assets.add("Water")
+        m.assets.add("AmbientLight", "sky blue", color=(1.6, 1.6, 1.8, 1))
+        m.assets.add("DirectionalLight", "sun light", color=(2.0, 1.9, 1.6, 1))
+        #m.assets.add("Sky", "bluesky")
+        #m.assets.add("Water")
 
-        m.assets.add("Aeroplane", "griffin", name="Griffin")
+        m.assets.add("Aeroplane", "Griffin", "griffin", True)
         griffin = m.assets.getLast()
         griffin.node.setZ(10)
-        griffin.physics.setVelocity(0, 300, 0)
+        griffin.physics.setVelocity(300)
         griffin.physics.setThrust(1)
 
-        m.views.activate("PlaneFlight", griffin)
-        m.controls.activate("PlaneFlight", griffin, m.views.getLast())
+        m.views.setView("PlaneView", griffin)
+        m.controls.addAndActivate("PlaneFlight", griffin, m.views.getView())
         # alternative syntax:
-        #pf = m.controls.add("PlaneFlight", griffin)
-        #m.controls.activate(pf)
-
-
-        # inline helper functions
-        getSpeed = lambda: round(griffin.physics.speed(), 1)
-        getThrust = lambda: round(getattr(griffin.physics, "thrust"), 1)
-
-        # on screen debugging
-        osd = debug.OSDebug(base.a2dTopLeft)
-        osd.add("speed", getSpeed)
-        osd.add("thrust", getThrust)
-        osd = debug.OSDebug(base.a2dBottomRight)
-        osd.add("x", lambda: int(griffin.node.getX()))
-        osd.add("y", lambda: int(griffin.node.getY()))
-        osd.add("z", lambda: int(griffin.node.getZ()))
+        # pf = m.controls.add("PlaneFlight", griffin)
+        # m.controls.activate(pf)
