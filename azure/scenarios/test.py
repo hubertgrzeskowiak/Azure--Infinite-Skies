9752007013dfd06be3e20d2ab8f721c76f0902bd
nemesis13 <dev at nemesis13.de>
2012-02-21 00:06:43 +0100

    use lists and dicts as extra args

--- azure/scenarios/test.py
+++ azure/scenarios/test.py
@@ -9,16 +9,15 @@ class Developmentenvironment(object):
 
     def begin(self, managers):
         m = managers
+        m.assets.add("AmbientLight", "sky blue", [], {"color":(1.6, 1.6, 1.8, 1)})
+        m.assets.add("DirectionalLight", "sun light", [], {"color":(2.0, 1.9, 1.6, 1)})
+        m.assets.add("Sky", "blue sky", ["bluesky"])
+        m.assets.add("Water", "water plane")
 
-        m.assets.add("AmbientLight", "sky blue", color=(1.6, 1.6, 1.8, 1))
-        m.assets.add("DirectionalLight", "sun light", color=(2.0, 1.9, 1.6, 1))
-        #m.assets.add("Sky", "bluesky")
-        #m.assets.add("Water")
-
-        m.assets.add("Aeroplane", "Griffin", "griffin", True)
+        m.assets.add("Aeroplane", "Griffin", ["griffin", True])
         griffin = m.assets.getLast()
         griffin.node.setZ(10)
-        griffin.physics.setVelocity(300)
+        #griffin.physics.setVelocity(300)
         griffin.physics.setThrust(1)
 
         m.views.setView("PlaneView", griffin)
